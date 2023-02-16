317dc7761096f61b06449199ad62f98d373a9ca4
888-coding <pachelee@gmail.com>
2023-02-16 07:36:26 -0300

    Counting variables

--- main.py
+++ main.py
@@ -5,6 +5,23 @@ print("Welcome to the Love Calculator")
 name1 = input("What is the first name ? \n")
 name2 = input("What is the second name ? \n")
 
-#Put together the both name
+#Put together the both name in lower case
 combine_name = str(name1).lower() + str(name2).lower()
 
+#Counting
+t = combine_name.count("t")
+r = combine_name.count("r")
+u = combine_name.count("u")
+e = combine_name.count("e")
+true = t + r + u + e
+
+l = combine_name.count("l")
+o = combine_name.count("o")
+v = combine_name.count("v")
+e = combine_name.count("e")
+love = l + o + v + e
+
+#fist digit is "true" , the second digit is "love"
+love_score = str(true) + str(love)
+
+print(love_score)
