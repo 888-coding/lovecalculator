d1953ecc8f74e87af94a7add6cb7e48f13ff37de
888-coding <pachelee@gmail.com>
2023-02-16 07:15:03 -0300

    App variables added

--- main.py
+++ main.py
@@ -2,6 +2,6 @@
 print("Welcome to the Love Calculator")
 
 #Adding the variables to program
-name1 = input("What is your name ? \n")
-name2 = input("What is your name ? \n")
+name1 = input("What is the first name ? \n")
+name2 = input("What is the second name ? \n")
 
